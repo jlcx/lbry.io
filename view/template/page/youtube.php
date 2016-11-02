a22e2af4675fcbe58c657b3eb08c36e9b7df015f
Jeremy Kauffman <kauffj@gmail.com>
2016-11-02 16:50:43 -0400

    fix favicon

--- view/template/page/youtube.php
+++ view/template/page/youtube.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" href="/img/youtube/favicon.ico" />
+    <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="57x57" href="/img/youtube/apple-touch-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/img/youtube/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="/img/youtube/apple-touch-icon-114x114.png">
